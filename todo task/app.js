bba1492245a9e4ef9e9b07478dceaa12946bd81e
suma511 <eliansumaia@gmail.com>
2022-01-23 23:49:10 +0200

    js2

--- todo task/app.js
+++ todo task/app.js
@@ -14,3 +14,51 @@ case "female":
     alert ("your gender is not valid");
 }
 
+
+
+function answer1 (A1){
+    if(A1=="yes" || "why")
+    return "yes" ;
+}
+
+function answer1 (A1){
+    if(A1=="no" || "n")
+    return "no" ; }
+
+    function answer2 (A2){
+        if(A2=="yes" || "why")
+        return "yes" ;
+    }
+
+    function answer2 (A2){
+        if(A2=="no" || "n")
+        return "no" ;
+    }
+
+    function answer3 (A3){
+        if(A3=="yes" || "why")
+        return "yes" ;
+    }
+
+    function answer3 (A1){
+        if(A3=="no" || "n")
+        return "no" ; }
+
+    functon invaled (A1,A2,A3) {
+    if (A1 =="" || A2 =="" || A3=="") {
+        Return prompt("invalid") ;
+    }
+
+        
+
+        let A1 = prompt("are u happy");
+        let A2 =prompt("are u sad") ;
+        let A3 =prompt("are u mad") ;
+
+        Answers [] ;
+
+  for (i=1 ; i<4 ;i+=)
+    {console.log(answers[i]);
+        ansewrs.push(A[i]);
+    }
+
