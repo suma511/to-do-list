91606bfe8433062da6d94ee9275ef1371e4378bd
sumayyaelian <97894473+suma511@users.noreply.github.com>
2022-01-23 19:09:18 +0200

    Update app.js
    edited

--- todo task/app.js
+++ todo task/app.js
@@ -1,13 +1,16 @@
-var username = prompt("enter your name");
-var userage = prompt("enter your age");
-if (userage<=0)
+var userName = prompt("enter your name");
+var userAge = prompt("enter your age");
+if (userAge<=0)
 alert("there is no age equal or less 0");
 var gender = prompt("enter your gender");
-if(gender="male")
-{
-alert("welcome Mr"+username);}
-else{
-alert("welcome Ms "+username);
+switch(gender)
+{ case "male":
+alert("welcome Mr" + userName);
+break;
+case "female":
+    alert ("welcome Ms" + userName);
+    break;
+    default:
+    alert ("your gender is not valid");
 }
-
 
